ac80c178b09bb73745fcf6430085668e3f1312b1
jeetendra1 <jeetendrapandey15@gmail.com>
2022-07-29 20:57:58 +0530

    create user api, and used jwt,  bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,56 @@
 const express = require("express");
-const User = require('../models/User');
+const User = require("../models/User");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
-//Create user using : POST '/api/auth'. Doesn't require auth
-router.post("/", (req, rep) => {
-  console.log(req.body);
-  const user = User(req.body);
-  user.save();
+const JWT_SECRET_KEY = 'dabli_priya';
 
-});
+//Create user using : POST '/api/auth/createuser'. Doesn't require auth. No login required
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter valid name").isLength({ min: 3 }),
+    body("email", "Enter valid email").isEmail(),
+    body("password", "Enter valid password").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    //if there are errors, return bad request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      //check whether user email is already exist
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry user with this email already exist." });
+      }
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
+      });
+      const data = {
+        user: {
+          id: user.id
+        }
+      }
+      var auttoken = jwt.sign(data, JWT_SECRET_KEY);
+
+      res.json({auttoken});
+    } catch (error) {
+      res.status(500).send(error.msg);
+    }
+    // .then(user => res.json(user))
+    // .catch(error => res.send(error));
+  }
+);
 
 module.exports = router;
--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, rep) => {
+router.get("/", (req, res) => {
   rep.send('Hi notes');
 
 });
